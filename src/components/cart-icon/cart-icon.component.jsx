bb8e5806abe02a1e8031e6ee50d5f2936ca065a8
Tina Gao <tina.hm.gao@gmail.com>
2020-03-06 17:38:43 +1300

    added checkout page, checkout item component and updated cart to use selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
 
 import { ReactComponent as BagIcon } from '../../assets/bag-icon.svg'
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({ toggleCartHidden }) => (
-  <div className="cart-icon" onClick={toggleCartHidden}>
+const CartIcon = ({ itemCount, dispatch }) => (
+  <div className="cart-icon" onClick={() => dispatch(toggleCartHidden())}>
     <BagIcon className="bag-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 )
 
-const mapDispatchToProps = dispatch => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden())
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps)(CartIcon)
